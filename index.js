b91dd75b11d86342e59a7fe78665b612014b9f0a
BrayanArroyoChavez <brayan.arroyo.chavez@gmail.com>
2021-02-23 17:33:12 -0600

    Modificación del formato de comentarios de código

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 /**
- * Author: Arroyo Chávez Brayan Alberto
- * Date: February 22, 2021
+ * Autor: Arroyo Chávez Brayan Alberto
+ * Fecha: February 22, 2021
  */
 
 /**
- * Se importa la libreria cheerio y axios
- * La libreria cheerio se utiliza para recolectar la información de la página destino
- * La libreria axios nos permite acceder a la página destino
+ * + Se importa la libreria cheerio y axios
+ * + La libreria cheerio se utiliza para recolectar la información de la página destino
+ * + La libreria axios nos permite acceder a la página destino
  */
 const cheerio = require('cheerio')
 const axios = require('axios');
 
 async function init(){
     /**
-     * En la constante html se guarda la página destino y la constante $ se guarda la página extraida 
+     * + En la constante html se guarda la página destino y la constante $ se guarda la página extraida 
      * convertida en un objetivo de cheerio.
-     * Se utiliza el await ya que es una operación que podria requerir de mucho tiempo y es necesario que 
+     * + Se utiliza el await ya que es una operación que podria requerir de mucho tiempo y es necesario que 
      * se concluya con la ejecución de estas dos operaciones antes de poder continuar.
      */
     const html = await axios.get('https://mx.investing.com/crypto/');
@@ -34,7 +34,7 @@ async function init(){
      * + Esto nos trae todo el codigo html de la tabla que se encuentra en la página.
      * + Para este caso se extrajeron tres valores en especifico name, symb y price son los nombres de las 
      * clases que contienen la información deseada, se hace uso de la función text para traer unicamente el
-     * texto encontrada en la etiqueta de la clase
+     * texto encontrada en la etiqueta de la clase.
      */
     $('tbody tr').each((i,el) =>{
         const name = $(el).find('.name').text();
