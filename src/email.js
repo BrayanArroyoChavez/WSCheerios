1f64f77dbbace667444dbd9a4820a7442eee005e
brayanarroyochavez <brayan.arroyo.chavez@gmail.com>
2021-02-25 20:38:43 -0600

    Cambio en el archivo email.js para que tomara los correos de la base de datos

--- src/email.js
+++ src/email.js
@@ -16,10 +16,33 @@ const connection = require('./connection')
 
 async function main() {
 
-  var sql = "SELECT FROM criptomonedas (name, symb, price, date) VALUES ('"+name+"', '"+symb+"','"+price+"','"+datetime+"')";
-    connection.con.query(sql, function (err, result) {
-        if (err) throw err;
+  var login = await getAuth();
+  var contact = await getContact();
+  var from = ""
+  contact.forEach(element => from = from.concat(',', element.email));
+  from = from.substr(1);
+
+  function getAuth() {
+    return dbQuery("SELECT p.email, u.password FROM `persona` AS p JOIN `persona_usuario` AS pu ON p.id = pu.id_persona JOIN `usuario` AS u ON u.id = pu.id_usuario WHERE p.type = 'admin'");
+  }
+
+  function getContact() {
+    return dbQuery("SELECT email FROM `persona` WHERE type = 'contacto'");
+  }
+
+  function dbQuery(databaseQuery) {
+    return new Promise(data => {
+      connection.con.query(databaseQuery, function (error, result) {
+        if (error) throw error;
+        try {
+          data(result);
+        } catch (error) {
+          data({});
+          throw error;
+        }
+      });
     });
+  }
 
   //crea un objeto transportador
   let transporter = nodemailer.createTransport({
@@ -27,31 +50,31 @@ async function main() {
   port: 587,
   secure: false,
   auth: {
-    user: "",
-    pass: ""
+    user: login[0].email,
+    pass: login[0].password
   }
   });
 
   //Busqueda del archivo handlebars
   const handlebarOptions = {
-      viewEngine: {
-        extName: ".handlebars",
-        partialsDir: path.resolve(__dirname, "views"),
-        defaultLayout: false,
-      },
-      viewPath: path.resolve(__dirname, "views"),
+    viewEngine: {
       extName: ".handlebars",
+      partialsDir: path.resolve(__dirname, "views"),
+      defaultLayout: false,
+    },
+    viewPath: path.resolve(__dirname, "views"),
+    extName: ".handlebars",
   };
     
   transporter.use(
-      "compile",
-      hbs(handlebarOptions)
+    "compile",
+    hbs(handlebarOptions)
   );
 
   // enviar correo con objeto de transporte definido
   let info = await transporter.sendMail({
-  from: "",
-  to: "",
+  from: login[0].email,
+  to: from,
   subject: "Prueba",
   template: "email"
   });
